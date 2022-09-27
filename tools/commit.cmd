7516cf5d3d4662e554a1212904e857ed1103ee75
kaffa <kaffacoffee@outlook.com>
2022-09-28 00:07:26 +0800

    'Kaffa'

--- tools/commit.cmd
+++ tools/commit.cmd
@@ -3,6 +3,7 @@ cd E:\code\github\kaffa\kaffa.github.io
 git add . 
 git commit -am 'Kaffa'
 git push
+python D:\code\py\py39\playwright_study\update_gitee_pages_20220109.py
 explorer https://kaffa.im
 explorer https://kaffa.github.io
 explorer https://kaffa.gitee.io
