aebaa78c9402261839ddd5d40028bded236b5fc7
kaffa <kaffacoffee@outlook.com>
2023-01-09 21:43:05 +0800

    'Kaffa'

---
+++ tools/aio.cmd
@@ -0,0 +1,9 @@
+E:
+cd E:\code\github\kaffa\kaffa.github.io
+pelican
+git add . 
+git commit -am 'Kaffa'
+git push
+explorer http://localhost:8000
+explorer https://kaffa.im
+explorer https://kaffa.github.io
