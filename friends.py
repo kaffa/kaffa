fa91ae947f0d114ad5f08151c30497e5ee6bf374
Kaffa <kaffacoffee@outlook.com>
2024-02-11 12:28:41 +0800

    'Kaffa'

--- friends.py
+++ friends.py
@@ -10,15 +10,22 @@ class Friend:
 
 
 def get_list():
-    friend_list = []
-
-    friend = Friend()
-    friend.ID = 1
-    friend.Name = '欧雷流'
-    friend.URL = 'https://ourai.ws/'
-    friend.Intro = "我叫欧雷，是一个出生于上世纪 80 年代的理想主义叛逆青年。喜欢日本，热爱咖啡，善于总结。这里有我对技术和语言的研究，也有对世界和生活的思考。"
-    friend.Owner = '欧雷'
-    friend.Icon = 'https://ourai.ws/assets/avatars/ourai-100px-3bcaa1cb0c7fa0547d15622572d74a8bbc98a5f62e4920ecddb72ca95e505d17.jpg'
-
-    friend_list.append(friend)
-    return friend_list
+    return [
+        {
+            'ID': 1,
+            'Name': '欧雷流',
+            'URL': 'https://ourai.ws/',
+            'Intro': '我叫欧雷，是一个出生于上世纪 80 年代的理想主义叛逆青年。喜欢日本，热爱咖啡，善于总结。这里有我对技术和语言的研究，也有对世界和生活的思考。',
+            'Owner': '欧雷',
+            'Icon': 'https://ourai.ws/assets/avatars/ourai-100px-3bcaa1cb0c7fa0547d15622572d74a8bbc98a5f62e4920ecddb72ca95e505d17.jpg',
+            'Email': ''
+        }, {
+            'ID': 2,
+            'Name': '浪海导航',
+            'URL': 'https://www.langhai.net/',
+            'Intro': '收录各种类型的博客',
+            'Owner': '浪海导航',
+            'Icon': 'https://www.langhai.net/assets/images/langhai-logo.png',
+            'Email': '676558206@qq.com'
+        }
+    ]
