e49e50050d31903465616baecadcadfaa6455581
Kaffa <kaffacoffee@outlook.com>
2024-03-07 12:53:57 +0800

    'Kaffa'

--- friends.py
+++ friends.py
@@ -13,6 +13,14 @@ def get_list():
     return [
         {
             'ID': 1,
+            'Name': '槿呈Goidea',
+            'URL': 'https://justgoidea.com/',
+            'Intro': '读书｜新知｜生活禅',
+            'Owner': '槿呈Goidea',
+            'Icon': 'https://cos.justgoidea.com/justgoidea%2Fblog-logo.webp',
+            'Email': 'me@hhzz.top',
+        }, {
+            'ID': 2,
             'Name': '欧雷流',
             'URL': 'https://ourai.ws/',
             'Intro': '我叫欧雷，是一个出生于上世纪 80 年代的理想主义叛逆青年。喜欢日本，热爱咖啡，善于总结。这里有我对技术和语言的研究，也有对世界和生活的思考。',
@@ -20,7 +28,7 @@ def get_list():
             'Icon': 'https://ourai.ws/assets/avatars/ourai-100px-3bcaa1cb0c7fa0547d15622572d74a8bbc98a5f62e4920ecddb72ca95e505d17.jpg',
             'Email': ''
         }, {
-            'ID': 2,
+            'ID': 3,
             'Name': '浪海导航',
             'URL': 'https://www.langhai.net/',
             'Intro': '收录各种类型的博客',
@@ -28,14 +36,12 @@ def get_list():
             'Icon': 'https://www.langhai.net/assets/images/langhai-logo.png',
             'Email': '676558206@qq.com'
         }, {
-            'ID': 3,
+            'ID': 4,
             'Name': '非理勿试',
             'URL': 'https://www.ntiy.com/',
             'Intro': '“Never Try It Yourself”，记录、分享、学习',
             'Owner': 'Soulizer、欧阳桂花、谢让',
             'Icon': '/static/img/2024/ntiy_logo_o.png',
             'Email': 'collect@live.com',
-            'QQ': '3018930',
-            'WX': '3018930(VIA_QQ)'
         }
     ]
