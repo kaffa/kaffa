7a27229136d0cd2b0bd6e544381dc780e1e4f9f3
Kaffa <kaffacoffee@outlook.com>
2024-03-13 02:08:57 +0800

    'Kaffa'

--- pelicanconf.py
+++ pelicanconf.py
@@ -59,7 +59,7 @@ LINKS = [
 # Social widget
 SOCIAL = (('My GitHub', 'https://github.com/kaffa'),)
 
-DEFAULT_PAGINATION = 6
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
