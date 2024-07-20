0e934dc8391f11fceb56ba1822760d54600f81a8
Kaffa <kaffacoffee@outlook.com>
2024-07-20 12:22:02 +0800

    'Kaffa'

--- tools/write-kaffa.im.py
+++ tools/write-kaffa.im.py
@@ -5,14 +5,9 @@ import json
 import subprocess
 
 
-def get_windows_username():
-    return os.environ.get('USERNAME')
-
-
-def main():
-    global GITHUB_LOCAL
-    username = get_windows_username()
-    file_path = f'C:\\Users\\{username}\\AppData\\Local\\JetBrains\\Toolbox\\apps\\PyCharm-C\\ch-0\\.history.json'
+def write(folder):
+    username = os.environ.get('USERNAME')
+    file_path = f'C:/Users/{username}/AppData/Local/JetBrains/Toolbox/apps/PyCharm-C/ch-0/.history.json'
 
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -21,9 +16,9 @@ def main():
             for item in history_list:
                 if 'system-install-path' in item['item']:
                     path = item['item']['system-install-path']
-                    subprocess.Popen(f"{path}\\bin\\pycharm64.exe {GITHUB_LOCAL}", shell=True)
+                    subprocess.Popen(f"{path}/bin/pycharm64.exe {folder}", shell=True)
 
 
 if __name__ == '__main__':
-    GITHUB_LOCAL = 'D:\\code\\github\\kaffa\\kaffa.github.io'
-    main()
+    github_local = 'D:/code/github/kaffa/kaffa.github.io'
+    write(github_local)
